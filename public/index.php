e6291aedf4585ca6d4709f0ee667647c3af87128
Uday Singh <33316@ma-web.nl>
2023-11-23 10:46:07 +0100

    finished

--- public/index.php
+++ public/index.php
@@ -3,8 +3,8 @@
 class Auto {
     private string $kleur;
     private int $zitplaatsen;
-    protected int $passagiers = 0; // Initieer passagiers op 0
-    protected int $snelheid = 0; // Initieer snelheid op 0
+    protected int $passagiers = 0;
+    protected int $snelheid = 0;
 
     public function __construct(string $kleur, int $zitplaatsen) {
         $this->kleur = $kleur;
@@ -13,10 +13,19 @@ class Auto {
 
     public function nieuwe_passagier(int $aantal) {
         $totalePassagiers = $this->passagiers + $aantal;
+
         if ($totalePassagiers <= $this->zitplaatsen && $aantal > 0) {
             $this->passagiers += $aantal;
+            // Verhoog de snelheid als er passagiers zijn toegevoegd
+            $this->snelheid += $aantal * 10; 
         } else {
-            echo "Fout: Ongeldig aantal passagiers of geen plek meer in de auto.<br>";
+            if ($totalePassagiers > $this->zitplaatsen) {
+                echo "Nee: Auto is vol, er zijn slechts {$this->zitplaatsen} zitplaatsen beschikbaar.<br>";
+            } elseif ($aantal <= 0) {
+                echo "Nee: Ongeldig aantal passagiers, aantal moet groter zijn dan 0.<br>";
+            } elseif ($this->passagiers > 4) {
+                echo "Nee: Maximaal 4 passagiers toegestaan voor de auto.<br>";
+            }
         }
     }
 
@@ -24,7 +33,7 @@ class Auto {
         if ($waarde > 0) {
             $this->snelheid += $waarde;
         } else {
-            echo "Fout: Ongeldige versnellingswaarde.<br>";
+            echo "Fout: fouten versnellingswaarde.<br>";
         }
     }
 
@@ -32,7 +41,7 @@ class Auto {
         if ($waarde > 0 && $this->snelheid - $waarde >= 0) {
             $this->snelheid -= $waarde;
         } else {
-            echo "Fout: Ongeldige remwaarde of auto staat al stil.<br>";
+            echo "Fout: fouten remwaarde of auto rijdt niet<br>";
         }
     }
 
@@ -42,6 +51,10 @@ class Auto {
 
     public function getSnelheid(): int {
         return $this->snelheid;
+    }
+
+    public function getKleur(): string {
+        return $this->kleur;
     }
 }
 
@@ -57,32 +70,50 @@ class Vrachtwagen extends Auto {
     public function lading(int $hoeveelheid) {
         if ($hoeveelheid > 0 && $this->lading + $hoeveelheid <= $this->laadvermogen) {
             $this->lading += $hoeveelheid;
+        } elseif ($hoeveelheid <= 0) {
+            echo "Fout: Ongeldige hoeveelheid lading, waarde moet groter zijn dan 0.<br>";
         } else {
-            echo "Fout: Ongeldige hoeveelheid lading of overbelasting van de vrachtwagen.<br>";
+            echo "Waarschuwing: Overbelasting van de vrachtwagen. Lading mag niet hoger zijn dan {$this->laadvermogen} kg.<br>";
+        }
+    }
+
+    public function nieuwe_passagier(int $aantal) {
+        if ($aantal === 0) {
+            echo "Waarschuwing: Er zijn geen passagiers in de vrachtwagen.<br>";
+        } elseif ($aantal > 2) {
+            echo "Waarschuwing: De vrachtwagen is vol met passagiers.<br>";
+        } else {
+            parent::nieuwe_passagier($aantal);
         }
     }
 
     public function versnel(int $waarde) {
         if ($waarde > 0) {
-            $this->snelheid += $waarde * 0.8; // Versnel de vrachtwagen met 80% van de waarde
+            $this->snelheid += $waarde; 
         } else {
             echo "Fout: Ongeldige versnellingswaarde voor vrachtwagen.<br>";
         }
     }
+    
 }
 
-// Voorbeeldgebruik:
-$auto = new Auto('rood', 5);
-$auto->nieuwe_passagier(4);
-$auto->versnel(50);
 
-echo "Aantal passagiers in de auto: " . $auto->getPassagiers() . "<br>";
-echo "Snelheid van de auto: " . $auto->getSnelheid() . " km/h<br>";
+$auto = new Auto('rood', 4);    //kies zitplaatsen
+$auto->nieuwe_passagier(1);     //pas de aantal van passagiers aan
+
+//bij 0 passagiers rijdt de voertuig niet dus is de snelheid 0
+
+
+echo "Kleur van de Auto: " . $auto->getKleur() . "<br>";
+echo "Aantal passagiers Auto: " . $auto->getPassagiers() . "<br>";
+echo "Snelheid van de Auto: " . $auto->getSnelheid() . " km/h<br>";
 
 $vrachtwagen = new Vrachtwagen('blauw', 3, 5000);
-$vrachtwagen->nieuwe_passagier(2);
-$vrachtwagen->lading(800);
-$vrachtwagen->versnel(40);
+$vrachtwagen->nieuwe_passagier(1);   //pas de aantal van passagiers aan (hoger dan 3 of lager dan 1 geeft een fout)
+$vrachtwagen->lading(1100);    // Dit zou een fout moeten geven
+$vrachtwagen->versnel(1);
 
-echo "Aantal passagiers in de vrachtwagen: " . $vrachtwagen->getPassagiers() . "<br>";
-echo "Snelheid van de vrachtwagen: " . $vrachtwagen->getSnelheid() . " km/h<br>";
+echo "Kleur van de Vrachtwagen: " . $vrachtwagen->getKleur() . "<br>";
+echo "Aantal passagiers in de Vrachtwagen: " . $vrachtwagen->getPassagiers() . "<br>";
+echo "Snelheid van de Vrachtwagen: " . $vrachtwagen->getSnelheid() . " km/h<br>";
+
