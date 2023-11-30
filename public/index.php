312184892ba16f20fc12f14cfaa77b2d884c748f
Uday Singh <33316@ma-web.nl>
2023-11-30 14:33:16 +0100

    added-lv-tailwind

--- public/index.php
+++ public/index.php
@@ -1,119 +1,55 @@
 <?php
 
-class Auto {
-    private string $kleur;
-    private int $zitplaatsen;
-    protected int $passagiers = 0;
-    protected int $snelheid = 0;
+use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Http\Request;
 
-    public function __construct(string $kleur, int $zitplaatsen) {
-        $this->kleur = $kleur;
-        $this->zitplaatsen = $zitplaatsen;
-    }
+define('LARAVEL_START', microtime(true));
 
-    public function nieuwe_passagier(int $aantal) {
-        $totalePassagiers = $this->passagiers + $aantal;
+/*
+|--------------------------------------------------------------------------
+| Check If The Application Is Under Maintenance
+|--------------------------------------------------------------------------
+|
+| If the application is in maintenance / demo mode via the "down" command
+| we will load this file so that any pre-rendered content can be shown
+| instead of starting the framework, which could cause an exception.
+|
+*/
 
-        if ($totalePassagiers <= $this->zitplaatsen && $aantal > 0) {
-            $this->passagiers += $aantal;
-            // Verhoog de snelheid als er passagiers zijn toegevoegd
-            $this->snelheid += $aantal * 10; 
-        } else {
-            if ($totalePassagiers > $this->zitplaatsen) {
-                echo "Nee: Auto is vol, er zijn slechts {$this->zitplaatsen} zitplaatsen beschikbaar.<br>";
-            } elseif ($aantal <= 0) {
-                echo "Nee: Ongeldig aantal passagiers, aantal moet groter zijn dan 0.<br>";
-            } elseif ($this->passagiers > 4) {
-                echo "Nee: Maximaal 4 passagiers toegestaan voor de auto.<br>";
-            }
-        }
-    }
-
-    public function versnel(int $waarde) {
-        if ($waarde > 0) {
-            $this->snelheid += $waarde;
-        } else {
-            echo "Fout: fouten versnellingswaarde.<br>";
-        }
-    }
-
-    public function rem(int $waarde) {
-        if ($waarde > 0 && $this->snelheid - $waarde >= 0) {
-            $this->snelheid -= $waarde;
-        } else {
-            echo "Fout: fouten remwaarde of auto rijdt niet<br>";
-        }
-    }
-
-    public function getPassagiers(): int {
-        return $this->passagiers;
-    }
-
-    public function getSnelheid(): int {
-        return $this->snelheid;
-    }
-
-    public function getKleur(): string {
-        return $this->kleur;
-    }
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
 }
 
-class Vrachtwagen extends Auto {
-    private int $laadvermogen;
-    private int $lading = 0; // Initialiseer lading op 0
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader for
+| this application. We just need to utilize it! We'll simply require it
+| into the script here so we don't need to manually load our classes.
+|
+*/
 
-    public function __construct(string $kleur, int $zitplaatsen, int $laadvermogen) {
-        parent::__construct($kleur, $zitplaatsen);
-        $this->laadvermogen = $laadvermogen;
-    }
+require __DIR__.'/../vendor/autoload.php';
 
-    public function lading(int $hoeveelheid) {
-        if ($hoeveelheid > 0 && $this->lading + $hoeveelheid <= $this->laadvermogen) {
-            $this->lading += $hoeveelheid;
-        } elseif ($hoeveelheid <= 0) {
-            echo "Fout: Ongeldige hoeveelheid lading, waarde moet groter zijn dan 0.<br>";
-        } else {
-            echo "Waarschuwing: Overbelasting van de vrachtwagen. Lading mag niet hoger zijn dan {$this->laadvermogen} kg.<br>";
-        }
-    }
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+|
+| Once we have the application, we can handle the incoming request using
+| the application's HTTP kernel. Then, we will send the response back
+| to this client's browser, allowing them to enjoy our application.
+|
+*/
 
-    public function nieuwe_passagier(int $aantal) {
-        if ($aantal === 0) {
-            echo "Waarschuwing: Er zijn geen passagiers in de vrachtwagen.<br>";
-        } elseif ($aantal > 2) {
-            echo "Waarschuwing: De vrachtwagen is vol met passagiers.<br>";
-        } else {
-            parent::nieuwe_passagier($aantal);
-        }
-    }
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
-    public function versnel(int $waarde) {
-        if ($waarde > 0) {
-            $this->snelheid += $waarde; 
-        } else {
-            echo "Fout: Ongeldige versnellingswaarde voor vrachtwagen.<br>";
-        }
-    }
-    
-}
+$kernel = $app->make(Kernel::class);
 
+$response = $kernel->handle(
+    $request = Request::capture()
+)->send();
 
-$auto = new Auto('rood', 4);    //kies zitplaatsen
-$auto->nieuwe_passagier(1);     //pas de aantal van passagiers aan
-
-//bij 0 passagiers rijdt de voertuig niet dus is de snelheid 0
-
-
-echo "Kleur van de Auto: " . $auto->getKleur() . "<br>";
-echo "Aantal passagiers Auto: " . $auto->getPassagiers() . "<br>";
-echo "Snelheid van de Auto: " . $auto->getSnelheid() . " km/h<br>";
-
-$vrachtwagen = new Vrachtwagen('blauw', 3, 5000);
-$vrachtwagen->nieuwe_passagier(1);   //pas de aantal van passagiers aan (hoger dan 3 of lager dan 1 geeft een fout)
-$vrachtwagen->lading(1100);    // Dit zou een fout moeten geven
-$vrachtwagen->versnel(1);
-
-echo "Kleur van de Vrachtwagen: " . $vrachtwagen->getKleur() . "<br>";
-echo "Aantal passagiers in de Vrachtwagen: " . $vrachtwagen->getPassagiers() . "<br>";
-echo "Snelheid van de Vrachtwagen: " . $vrachtwagen->getSnelheid() . " km/h<br>";
-
+$kernel->terminate($request, $response);
